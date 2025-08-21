519b9c8f13d6b8e28b6a97ae392d71ad94ee22ba
Andrewmang <sangsinmang97@gmail.com>
2025-08-21 13:51:21 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -7,6 +7,10 @@ public class HelloWorld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
-        System.out.println("Hello, World");
+        System.out.println("My Name is Andrew");
+        System.out.println("Course Name: Object Oriented Programming In Java1");
+        System.out.println("Course Code: CSC 200");
+         System.out.println("Lecturer Name: Sathish Sarma");           
+      
     }
 }
